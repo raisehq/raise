f44c9168ac77497868d0afd529888a93e32dee56
Kartojal <canillas.mail@gmail.com>
2019-07-26 10:33:09 +0200

    LP-0 Fix import

--- packages/client/src/components/Join/index.tsx
+++ packages/client/src/components/Join/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Onboarding from '../Onboarding';
+import Onboarding from '@raise/onboarding';
 
 const Join = ({ history }) => <Onboarding open={true} history={history} />;
 
