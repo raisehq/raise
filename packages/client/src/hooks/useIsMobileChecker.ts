e761c6d1dcc2be8fc4f92f4118237c9f1c883b79
alvaroherotoken <alvaro@herotoken.io>
2019-08-02 13:01:57 +0200

    LP-543 fix Mobile buttons

--- packages/client/src/hooks/useIsMobileChecker.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from "react";
-
-const useIsMobileChecker = () => 
- useMemo(() => /(windows phone|android|iphone|ipad|mobile)/i.test(
-    navigator.userAgent
-  ), []);
-;
-
-export default useIsMobileChecker;
