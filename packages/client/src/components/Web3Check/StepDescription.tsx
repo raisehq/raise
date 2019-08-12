07b82cf7715dfa5182bae2ee732d8024cb8667b3
Kartojal <canillas.mail@gmail.com>
2019-08-12 15:19:55 +0200

    LP-589 add new copy

--- packages/client/src/components/Web3Check/StepDescription.tsx
+++ packages/client/src/components/Web3Check/StepDescription.tsx
@@ -25,13 +25,14 @@ const BrowserCompatible = () => (
   <CardDescription>
     <BrowserView>
       <p>To access Raise you will need a browser that supports MetaMask:
-      <span>
-        <a href='https://www.mozilla.org/firefox'> Firefox</a>
-        <span>, </span>
-        <a href='https://www.google.com/chrome'>Chrome</a>
-        <span> and </span>
-        <a href='https://brave.com/'>Brave</a>.
-      </span>
+        <span>
+          <a href='https://www.mozilla.org/firefox'> Firefox</a>
+          <span>, </span>
+          <a href='https://www.google.com/chrome'>Chrome</a>
+          <span> and </span>
+          <a href='https://brave.com/'>Brave</a>.
+        </span>
+      </p>
     </BrowserView>
     <MobileView>
       <p>To access Raise on mobile please download <a href='https://mobile.metamask.io/Metamask'>Metamask Mobile</a>.</p>
