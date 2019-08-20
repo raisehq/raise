1ead9fc469c2933d1d18e093441b40b6300427dc
Raul Vallespin <raul@herotoken.io>
2019-08-20 13:12:57 +0200

    LP-609 Base template for both dashboards

--- packages/client/src/components/Web3Check/Web3Route.tsx
+++ packages/client/src/components/Web3Check/Web3Route.tsx
@@ -1,38 +1,45 @@
 import React, { useContext } from 'react';
 import { withRouter, Redirect } from 'react-router-dom';
-import {AppContext} from '../App';
+import { AppContext } from '../App';
 import { NotAllowed } from '../NotAllowed';
 
-const Route = (props) => {
+const Route = props => {
   const { history, layout, exact, roles, ...rest }: any = props;
   const {
     store: {
-      auth: { login: { logged } },
-      user: { details: { accounttype_id } }
+      auth: {
+        login: { logged }
+      },
+      user: {
+        details: { accounttype_id }
+      }
     },
-    web3Status : { accountMatches: accMatch, networkMatches: netOk }
+    web3Status: { accountMatches: accMatch, networkMatches: netOk }
   }: any = useContext(AppContext);
 
   const web3Pass = netOk && accMatch;
-  const refMode = (process.env.REACT_APP_REFERAL == 'true');
+  const refMode = process.env.REACT_APP_REFERAL == 'true';
   const Layout = layout;
-  
-  const acceptedRole = roles !== undefined && roles.indexOf(accounttype_id) > -1 || false;
+
+  const acceptedRole =
+    (roles !== undefined && roles.indexOf(accounttype_id) > -1) || false;
 
   if (logged && web3Pass && acceptedRole) {
-    return <Layout exact {...rest} />
+    return <Layout exact {...rest} />;
   }
-  
+
   if (logged && web3Pass && !acceptedRole) {
-    return <Redirect to='/dashboard' />
+    return <Redirect to="/dashboard" />;
   }
 
   if (logged && !web3Pass && history.location.pathname !== '/verify-web3') {
-    const redirectUrl = refMode ? '/verify-web3' : `/verify-web3?redirect=${history.location.pathname}`;
-    return <NotAllowed to={redirectUrl} />
+    const redirectUrl = refMode
+      ? '/verify-web3'
+      : `/verify-web3?redirect=${history.location.pathname}`;
+    return <NotAllowed to={redirectUrl} />;
   }
-  
+
   return null;
-}
+};
 
 export default withRouter(Route);
