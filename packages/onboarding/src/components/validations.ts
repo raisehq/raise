ff93b97a316d1f3e149c9ec73aa7067840180c24
Daniel <daniel@herotoken.io>
2019-08-22 17:27:17 +0200

    LP-477

--- packages/onboarding/src/components/validations.ts
+++ packages/onboarding/src/components/validations.ts
@@ -1,8 +1,8 @@
-import isEmail from 'validator/lib/isEmail'
-import isLength from 'validator/lib/isLength'
-import { Either } from '../utils'
+import isEmail from 'validator/lib/isEmail';
+import isLength from 'validator/lib/isLength';
+import { Either } from '../utils';
 
 export default {
   isEmail: value => Either.either(isEmail(value)),
   password: value => Either.either(isLength(value, { min: 8 }))
-}
+};
