7cb931e6a1b4441e8f47c3370f1dabfa601db644
David C <david.c@hero-fintech.com>
2019-08-23 13:52:20 +0200

    LP-364 delete test modal in marketplace

--- packages/client/src/components/Marketplace/Marketplace.tsx
+++ packages/client/src/components/Marketplace/Marketplace.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { InvestModal } from '../InvestModal';
 
 const Marketplace = () => {
-  const loan = {
-    id: '0x5b981d623f5a5d99fe32bde7e727646eeea411c5',
-    principal: '0',
-    maxAmount: '10000000000000000000000',
-    operatorFee: '1000000000000000000',
-    termEndTimestamp: '1574418420088',
-    netBalance: null,
-    auctionEnded: false,
-    auctionEndBlock: '13595305',
-    interestRate: '5',
-    borrowerDebt: '0',
-    investorCount: 1
-  };
   return (
     <div>
-      HOLA FROM Marketplace
-      <InvestModal loan={loan} />
+      Marketplace
     </div>
   );
 };
