a66ce86729c49a32bbebe71a51012803866b888a
David C <david.c@hero-fintech.com>
2019-08-26 15:14:16 +0200

    LP-364 change time in modal

--- packages/client/src/components/InvestModal/InvestModal.tsx
+++ packages/client/src/components/InvestModal/InvestModal.tsx
@@ -3,13 +3,14 @@ import useAsyncEffect from '../../hooks/useAsyncEffect';
 import useMetamask from '../../hooks/useMetaMask';
 import { Modal as SemanticModal } from 'semantic-ui-react';
 import { fromWei, toWei } from 'web3-utils';
-import { getWeb3, averageBlockTime } from '../../utils';
+import { getWeb3 } from '../../utils';
 import { TokenInput } from '../TokenInput';
 import numeral from '../CreateLoan/numeral';
 import ERC20 from '../../commons/erc20';
 import { MAX_VALUE } from '../../commons/constants';
 import { numeralFormat } from '../CreateLoan/numeral';
 import { ResumeItemProps, RaisedAmountProps, InvestModalProps } from './types';
+import useCalc from '../Dashboard/Dashboard.useCalc';
 import {
   LenderButton,
   Modal,
@@ -43,53 +44,14 @@ const RaisedAmount: React.SFC<RaisedAmountProps> = ({ value }) => (
   </RaisedAmountBox>
 );
 
-const getAuctionEndDate = auctionRemainingSeconds => {
-  let now = new Date();
-  now.setSeconds(now.getSeconds() + auctionRemainingSeconds);
-
-  return now;
-};
-
-const getTermLength = (termEndDate, auctionEndDate) => {
-  let diff = (termEndDate.getTime() - auctionEndDate.getTime()) / 1000;
-  diff /= 60 * 60 * 24 * 7 * 4;
-
-  return Math.abs(Math.round(diff));
-};
-
-const calculateTerms = async auction => {
-  try {
-    const web3 = getWeb3();
-    const currentBlock = await web3.eth.getBlockNumber();
-    const averageBT = await averageBlockTime();
-    const auctionRemainingBlocks = auction.auctionEndBlock - currentBlock;
-    const auctionRemainingSeconds = auctionRemainingBlocks * averageBT;
-    const auctionEndDate = getAuctionEndDate(auctionRemainingSeconds);
-    const termEndDate = new Date(parseInt(auction.termEndTimestamp));
-    return {
-      loanDuration: getTermLength(termEndDate, auctionEndDate),
-      auctionEnds: Math.round(auctionRemainingSeconds / 60 / 60 / 24)
-    };
-  } catch (error) {
-    return error;
-  }
-};
-
 const InvestModal: React.SFC<InvestModalProps> = ({ loan }) => {
   const { id: loanAddress, principal, investorCount, maxAmount } = loan;
+  const { times } = useCalc(loan);
   const {
     web3Status: { account }
   }: any = useContext(AppContext);
   const metamask = useMetamask();
   const [open, setOpen] = useState(false);
-  const [auctionDuration, setAuctionDuration]: [
-    number,
-    React.Dispatch<React.SetStateAction<number>>
-  ] = useState(0);
-  const [loanTerm, setLoanTerm]: [
-    number,
-    React.Dispatch<React.SetStateAction<number>>
-  ] = useState(0);
   const [value, setValue]: [
     number,
     React.Dispatch<React.SetStateAction<number>>
@@ -110,12 +72,8 @@ const InvestModal: React.SFC<InvestModalProps> = ({ loan }) => {
           .getInterestRate()
           .call();
         setInterest(Number(loanInterest) / 1000);
-        const { loanDuration, auctionEnds } = await calculateTerms(loan);
-        setLoanTerm(loanDuration);
-        setAuctionDuration(auctionEnds);
       } catch (error) {
-        setInterest(0);
-        setLoanTerm(0);
+        console.error(error);
       }
     }
   }, [metamask, loanAddress]);
@@ -198,12 +156,12 @@ const InvestModal: React.SFC<InvestModalProps> = ({ loan }) => {
             <RaisedAmount value={raised} />
             <FlexSpacedLayout>
               <ResumeItem title="Borrower" value="Company A" />
-              <ResumeItem title="Loan Term" value={`${loanTerm} months`} />
+              <ResumeItem title="Loan Term" value={`${times.loanTerm}`} />
               <ResumeItem title="Min APR" value={`${interest} %`} />
 
               <ResumeItem title="Target Amount" value={`${targetAmount} DAI`} />
               <ResumeItem title="Investors" value={`${investorCount}`} />
-              <ResumeItem title="Days left" value={`${auctionDuration}`} />
+              <ResumeItem title="Time left" value={`${times.auctionTimeLeft}`} />
             </FlexSpacedLayout>
             <ProgressLayout>
               <AuctionProgress active percent={raisedPercentage} />
