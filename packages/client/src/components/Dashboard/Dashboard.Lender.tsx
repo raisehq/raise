6639e6cfc77b0393a953da1c0afb65fefc21e9ad
pol <pol@hero-fintech.com>
2019-09-01 23:49:41 +0200

    LP-652 first version working

--- packages/client/src/components/Dashboard/Dashboard.Lender.tsx
+++ packages/client/src/components/Dashboard/Dashboard.Lender.tsx
@@ -1,29 +1,56 @@
 import React, { useContext, useCallback, useEffect } from 'react';
 import { Header } from 'semantic-ui-react';
-import { Button, DashboardContainer, DashboardWrapper } from './Dashboard.styles';
+// import gql from 'graphql-tag';
+import { Button, DashboardContainer, DashboardWrapper, DashboardTab } from './Dashboard.styles';
 import KycMessage from '../KycMessage';
 import { AppContext } from '../App';
 import Suggesteds from './Dashboard.Suggesteds';
 import Tab from './Dashboard.Tab';
-import { DashboardTab } from './Dashboard.styles';
+import Queryies from '../../helpers/queryies';
 
 const Dashboard = () => {
   const {
     history,
     actions: {
-      loan: { onGetSuggestedAuctions, onGetLiveAuctionsByAccount }
+      loan: { onGetSuggestedAuctionsSubscription, onGetLiveAuctionsByAccountSubscription }
     },
     store: {
-      loan: { suggested, auctions }
-    }
+      loan: { suggested, auctions },
+      user: {
+        cryptoAddress: { address }
+      }
+    },
+    webSocket: { webSocket }
   }: any = useContext(AppContext);
 
   const onCreateLoan = useCallback(() => history.push('/marketplace'), []);
 
+  // useEffect(() => {
+  //   onGetSuggestedAuctions();
+  //   onGetLiveAuctionsByAccount();
+  // }, []);
+
   useEffect(() => {
-    onGetSuggestedAuctions();
-    onGetLiveAuctionsByAccount();
-  }, []);
+    console.log('websocket:: ', webSocket);
+    if (webSocket) {
+      const { query, subscriptionName } = Queryies.subscriptions.liveAuctionsByAccount;
+      const variables = {
+        address
+      };
+      const callback = onGetLiveAuctionsByAccountSubscription;
+      webSocket.subscribe(query, variables, subscriptionName, callback);
+    }
+  }, [webSocket]);
+
+  useEffect(() => {
+    console.log('websocket:: ', webSocket);
+    if (webSocket) {
+      const { query, variables, subscriptionName } = Queryies.subscriptions.lenderSuggestions;
+
+      const callback = onGetSuggestedAuctionsSubscription;
+      webSocket.subscribe(query, variables, subscriptionName, callback);
+    }
+  }, [webSocket]);
 
   const panes = [
     {
@@ -32,7 +59,7 @@ const Dashboard = () => {
     },
     {
       menuItem: 'Investments',
-      render: () => <Tab auctions={auctions} states={[1,2,3,4,5,6]} type="auction" />
+      render: () => <Tab auctions={auctions} states={[1, 2, 3, 4, 5, 6]} type="auction" />
     }
   ];
   return (
@@ -40,7 +67,7 @@ const Dashboard = () => {
       <KycMessage />
       <DashboardContainer>
         <Header as="h1">Suggested auctions</Header>
-        <Suggesteds auctions={suggested} states={[0]}/>
+        <Suggesteds auctions={suggested} states={[0]} />
         <Button onClick={onCreateLoan}>marketplace</Button>
         <Header as="h1">My activity</Header>
         <DashboardTab renderActiveOnly menu={{ secondary: true, pointing: true }} panes={panes} />
--- packages/client/src/store/actions/loan.ts
+++ packages/client/src/store/actions/loan.ts
@@ -2,15 +2,9 @@ import { getLiveAuctionsByAccount, getSuggestedAuctions } from '../../services/l
 
 export default (dispatch: any, state: any) => {
   const onGetLiveAuctionsByAccount = async () => {
-    const request = await getLiveAuctionsByAccount(
-      state.user.cryptoAddress.address,
-      'kovan'
-    );
+    const request = await getLiveAuctionsByAccount(state.user.cryptoAddress.address, 'kovan');
 
-    request.fold(
-      () => null,
-      auctions => dispatch({ type: 'SET_LIVE_AUCTIONS', data: auctions })
-    );
+    request.fold(() => null, auctions => dispatch({ type: 'SET_LIVE_AUCTIONS', data: auctions }));
   };
 
   const onGetSuggestedAuctions = async () => {
@@ -22,7 +16,27 @@ export default (dispatch: any, state: any) => {
     );
   };
 
+  const onGetSuggestedAuctionsSubscription = (error, data) => {
+    if (error) {
+      console.log('error on get sugg subs :: ', error);
+    } else {
+      console.log('data is =====================> ', data);
+      dispatch({ type: 'SET_SUGGESTED_AUCTIONS', data: data.loans });
+    }
+  };
+
+  const onGetLiveAuctionsByAccountSubscription = (error, data) => {
+    if (error) {
+      console.log('error on get live auction subs :: ', error);
+    } else {
+      console.log('data is auction live =====================> ', data.users[0].loanRequests);
+      dispatch({ type: 'SET_LIVE_AUCTIONS', data: data.users[0].loanRequests });
+    }
+  };
+
   return {
+    onGetSuggestedAuctionsSubscription,
+    onGetLiveAuctionsByAccountSubscription,
     onGetLiveAuctionsByAccount,
     onGetSuggestedAuctions
   };
