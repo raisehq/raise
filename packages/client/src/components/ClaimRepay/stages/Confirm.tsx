7089d45e4295decfcb882dea3223d17474ce5bcd
David C <david.c@hero-fintech.com>
2019-09-06 11:42:51 +0200

    LP-374 Claim repayment modal

--- packages/client/src/components/ClaimRepay/stages/Confirm.tsx
+++ packages/client/src/components/ClaimRepay/stages/Confirm.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { Loader } from 'semantic-ui-react';
 import { Header, BorrowerButton } from '../../InvestModal/InvestModal.styles';
 import { ClaimRepayContext } from '../ClaimRepay';
-import { getCalculations } from '../../../utils/loanUtils'
+import { getCalculations } from '../../../utils/loanUtils';
 import numeral from '../../../commons/numeral';
 import { fromWei } from 'web3-utils';
 import {
@@ -28,31 +28,29 @@ const ResumeItemBig: React.SFC<ResumeItemProps> = ({ title, value }) => (
 );
 
 const Confirm = () => {
-  const { claimRepayment, loan, pending }: any = useContext(ClaimRepayContext)
+  const { claimRepayment, loan, pending }: any = useContext(ClaimRepayContext);
 
   const { roi }: any = getCalculations(loan);
   const lenderAmount = numeral(fromWei(loan.lenderAmount)).format();
   const lenderRoiAmount = numeral(
-    Number(fromWei(loan.lenderAmount)) +
-      Number(fromWei(loan.lenderAmount)) * numeral(roi).value()
+    Number(fromWei(loan.lenderAmount)) + Number(fromWei(loan.lenderAmount)) * numeral(roi).value()
   ).format();
 
   return (
     <>
-    <Header>Claim funds</Header>
-    <ClaimFundsResume>
-      <FlexSpacedLayout>
-        <ResumeItem title="Amount invested" value={`${lenderAmount} DAI`} />
-        <ResumeItem title="ROI" value={`${roi}`} />
-        <ResumeItemBig title="Total repayment amount" value={`${lenderRoiAmount} DAI`} />
-      </FlexSpacedLayout>
-
-    </ClaimFundsResume>
-    <Loader active inverted />
-    <BorrowerButton loading={pending} onClick={claimRepayment}>
-      Repay
-    </BorrowerButton>
-  </>
+      <Header>Claim funds</Header>
+      <ClaimFundsResume>
+        <FlexSpacedLayout>
+          <ResumeItem title="Amount invested" value={`${lenderAmount} DAI`} />
+          <ResumeItem title="ROI" value={`${roi}`} />
+          <ResumeItemBig title="Total repayment amount" value={`${lenderRoiAmount} DAI`} />
+        </FlexSpacedLayout>
+      </ClaimFundsResume>
+      <Loader active inverted />
+      <BorrowerButton loading={pending} onClick={claimRepayment}>
+        Repay
+      </BorrowerButton>
+    </>
   );
 };
 
--- packages/client/src/components/ClaimRepay/types.ts
+++ packages/client/src/components/ClaimRepay/types.ts
@@ -3,32 +3,3 @@ import { Loan } from '../../commons/loan';
 export interface InvestModalProps {
   loan: Loan;
 }
-
-export interface InvestStateProps {
-  loan: Loan;
-  setStage: Function;
-  setInvestment: Function;
-  ui: any;
-}
-
-export interface ResumeItemProps {
-  value: string;
-  title: string;
-}
-
-export interface RaisedAmountProps {
-  value: any;
-}
-
-export interface ProcessingStateProps {
-  loan: Loan;
-  investment: any;
-  ui: any;
-  setStage: Function;
-}
-
-export interface SuccessStateProps {
-  setStage: Function;
-  closeModal: Function;
-  ui: any;
-}
