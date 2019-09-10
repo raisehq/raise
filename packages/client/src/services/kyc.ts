010d293e327c838b9c2355c3a496c683c481b28a
pol <pol@hero-fintech.com>
2019-09-10 18:10:43 +0200

    LP-681 added headers so kyc validation token works

--- packages/client/src/services/kyc.ts
+++ packages/client/src/services/kyc.ts
@@ -1,16 +1,19 @@
 import axios from 'axios';
 import { to } from '../utils';
+import Header from '../helpers/header';
 
 const HOST = `http://localhost:10000/api/kyc`;
+
+const COMMON_HEADERS = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
+};
 
 export const initKyc = async id => {
   const config: any = {
     url: `${HOST}/auth/${id}`,
     method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
+    headers: { ...COMMON_HEADERS, ...Header.getHeaderAuth() }
   };
 
   const request = await to(axios(config));
@@ -22,10 +25,7 @@ export const connect = async (userId, email, token) => {
   const config: any = {
     url: `${HOST}/connect/${userId}/${email}/${token}`,
     method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
+    headers: { ...COMMON_HEADERS, ...Header.getHeaderAuth() }
   };
 
   const request = await to(axios(config));
@@ -37,10 +37,7 @@ export const isKYCVerified = async (userId, token) => {
   const config: any = {
     url: `${HOST}/isverified/${userId}/${token}`,
     method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
+    headers: { ...COMMON_HEADERS, ...Header.getHeaderAuth() }
   };
 
   const request = await to(axios(config));
