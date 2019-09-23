3537c1ded746e162063a481780d498c1b92763c9
Raul Vallespin <raul@herotoken.io>
2019-09-23 13:53:35 +0200

    LP-701 init

---
+++ packages/client/src/components/GetStarted/GetStarted.styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  border: 1px solid #cfd0d4;
+  border-radius: 6px;
+  margin: 25px 0 25px 0;
+  padding: 15px;
+  box-sizing: border-box;
+`;
