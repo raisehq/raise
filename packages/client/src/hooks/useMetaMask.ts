0df1740387d4e40080e3481c6018d66a7d8891a3
Daniel Martin <daniel@herotoken.io>
2019-10-22 06:33:14 +0200

    LP-532 is dapper

--- packages/client/src/hooks/useMetaMask.ts
+++ packages/client/src/hooks/useMetaMask.ts
@@ -46,17 +46,50 @@ const useMetaMask = () => {
           if (!hasIn(heroContracts, `abi.${name}`)) {
             throw new Error(`contract not found in abi metadata list`);
           }
-          const contract = new web3.eth.Contract(
-            get(heroContracts, `abi.${name}`),
-            address
-          );
+          const contract = new web3.eth.Contract(get(heroContracts, `abi.${name}`), address);
           contracts.current = { ...contract.current, [name]: contract };
           return contract;
         },
         utils: web3.utils,
         getContract: (name: string) => contracts.current[name],
         getContracts: () => contracts.current,
-        getAccounts: web3.eth.getAccounts
+        getAccounts: web3.eth.getAccounts,
+        getCurrentProviderName: () => {
+          if (!web3) return 'unknown';
+
+          if (web3.currentProvider.isMetaMask) return 'metamask';
+
+          if (web3.currentProvider.isDapper) return 'dapper';
+
+          if (web3.currentProvider.isTrust) return 'trust';
+
+          if (web3.currentProvider.isGoWallet) return 'goWallet';
+
+          if (web3.currentProvider.isAlphaWallet) return 'alphaWallet';
+
+          if (web3.currentProvider.isStatus) return 'status';
+
+          if (web3.currentProvider.isToshi) return 'coinbase';
+          //  @ts-ignore
+          // eslint-disable-next-line
+          if (typeof window.__CIPHER__ !== 'undefined') return 'cipher';
+
+          if (web3.currentProvider.constructor.name === 'EthereumProvider') return 'mist';
+
+          if (web3.currentProvider.constructor.name === 'Web3FrameProvider') return 'parity';
+          // eslint-disable-next-line
+          if (web3.currentProvider.host && web3.currentProvider.host.indexOf('infura') !== -1) {
+            // eslint-disable-next-line
+            return 'infura';
+          }
+
+          if (web3.currentProvider.host && web3.currentProvider.host.indexOf('localhost') !== -1) {
+            // eslint-disable-next-line
+            return 'localhost';
+          }
+
+          return 'unknown';
+        }
       });
     }
   }, [web3, network, heroContracts]);
