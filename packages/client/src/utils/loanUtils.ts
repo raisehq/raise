343a2d5254afbd1d8da7304adfa2f27d404267c0
Pol Sendra <pol@hero-fintech.com>
2019-11-13 14:44:52 +0000

    LP-794 fixed bug that passed string instead of number to numeral transformer

--- packages/client/src/utils/loanUtils.ts
+++ packages/client/src/utils/loanUtils.ts
@@ -180,7 +180,7 @@ export const getCalculations = auction => {
     totalInterestAmount = numeral(calculateTotalInterestAmount(auction)).format();
   }
   if (auction.lenderAmount) {
-    lenderAmount = numeral(fromWei(auction.lenderAmount)).format();
+    lenderAmount = numeral(Number(fromWei(auction.lenderAmount))).format();
     const lenderRoiAmountCalc = calculateInvestmentReturn(auction);
     lenderRoiAmount = numeral(lenderRoiAmountCalc).format();
   }
