c98829d42e644e5d00cc0a69c5b223d4fdf25081
David Canillas Racero <david.c@hero-fintech.com>
2019-11-27 11:05:24 +0100

    LP-877 Added card to client

--- packages/client/src/components/Cards/Suggested.tsx
+++ packages/client/src/components/Cards/Suggested.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { InvestCard, InvestButton } from './Suggested.styles';
-import butterInstance from '../../helpers/butter';
+import useBorrowerInfo from '../../hooks/useBorrowerInfo';
 
 interface LoanProps {
   auction: any;
 }
 
 const Loan: React.SFC<LoanProps> = ({ auction }: LoanProps) => {
+  const borrower = useBorrowerInfo(auction.originator);
   return (
-    <InvestCard auction={auction} butter={butterInstance}>
+    <InvestCard auction={auction} borrower={borrower} link>
       <InvestButton loan={auction} />
     </InvestCard>
   );
