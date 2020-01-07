53235fccadbe5fd6a1001093df915c494efdc1fb
Daniel Martin <daniel@herotoken.io>
2020-01-07 11:28:01 +0100

    LP-944 fix title

--- packages/client/src/components/Web3Check/Web3Check.Wallet.tsx
+++ packages/client/src/components/Web3Check/Web3Check.Wallet.tsx
@@ -60,7 +60,7 @@ const Wallet = ({ onNext }: any) => {
       <OnboardingProgressBar step={1} isMobile={isMobile} />
       <SelectYourWalletContainer>
         <SelectYourWalletTitle>
-          <CardTitle>Select your wallet </CardTitle>
+          <CardTitle>Select your wallet</CardTitle>
           <CardSubTitle>Get started by connecting one of the wallets below</CardSubTitle>
         </SelectYourWalletTitle>
         <SelectYourWalletList>
