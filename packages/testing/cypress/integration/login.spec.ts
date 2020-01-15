7c8da3cd3bbc8919ff2afa0eff945d326c5d00ce
David Canillas Racero <david.c@hero-fintech.com>
2020-01-15 16:27:09 +0100

    LP-979 added slash in url, remoce slash in cypress base env variable

--- packages/testing/cypress/integration/login.spec.ts
+++ packages/testing/cypress/integration/login.spec.ts
@@ -1,11 +1,11 @@
 describe('Login', function() {
   it('Make Login', function() {
     cy.web3('lender');
-    cy.visit(`${Cypress.env('url')}login`);
+    cy.visit(`${Cypress.env('url')}/login`);
     cy.wait(2000);
+    cy.get('.process').matchImageSnapshot('login_modal');
     const user = Cypress.env('user');
     cy.get('#input-login').should('have.length', 1);
-    cy.get('.process').matchImageSnapshot('login_modal');
     cy.get('#input-login').type(user['lender'].email);
     cy.get('#input-password').should('have.length', 1);
     cy.get('#input-password').type(user['lender'].password);
