4238cd9aed1c59b9d785eda45552b22e4d0d8c09
gastonhero <gaston@hero-fintech.com>
2020-01-17 16:36:06 +0100

    Fix tests2

--- packages/testing/cypress/integration/login.spec.ts
+++ packages/testing/cypress/integration/login.spec.ts
@@ -5,14 +5,14 @@ describe('Login', function() {
     cy.wait(2000);
     cy.get('#btn-sign-in-email').click();
     cy.wait(2000);
-    cy.get('#process').matchImageSnapshot('login_modal');
+    cy.get('.process').matchImageSnapshot('login_modal');
     const user = Cypress.env('user');
     cy.get('#input-login').should('have.length', 1);
     cy.get('#input-login').type(user['lender'].email);
     cy.get('#input-password').should('have.length', 1);
     cy.get('#input-password').type(user['lender'].password);
     cy.wait(1000);
-    cy.get('#process').matchImageSnapshot('login_modal_filled');
+    cy.get('.process').matchImageSnapshot('login_modal_filled');
     cy.get('#btn-login').click();
   });
 });
