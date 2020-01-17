12e310a0bc819dd9d533a1baee19baa1954fc6b6
gastonhero <gaston@hero-fintech.com>
2020-01-17 15:11:29 +0100

    More changes

--- packages/onboarding/src/components/SignIn/SignIn.tsx
+++ packages/onboarding/src/components/SignIn/SignIn.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import { ChooseMethodWrapper } from './styles';
+import React, { useContext } from 'react';
+import { IContext } from '../types';
+import AppContext from '../App.context';
 import AuthenticationMethods from '../AuthenticationMethods/AuthenticationMethods';
+import { ChooseMethodWrapper } from './styles';
+import { ChooseSignUpSignInWrapper, CallToSignIn } from '../styles';
 
 const Signin = () => {
+  const { onSetStep } = useContext<IContext>(AppContext);
+
   return (
+    <ChooseMethodWrapper>
       <AuthenticationMethods method={'Sign In'} />
+      <ChooseSignUpSignInWrapper>
+        <CallToSignIn>
+          Don't you have an account?
+          <button className="callToSignIn" type="button" onClick={() => onSetStep('Start')()}>
+            Sign Up
+          </button>
+        </CallToSignIn>
+      </ChooseSignUpSignInWrapper>
+    </ChooseMethodWrapper>
   );
 };
 
--- packages/onboarding/src/components/SignUp/GetStarted.tsx
+++ packages/onboarding/src/components/SignUp/GetStarted.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import AppContext from '../App.context';
 import { IContext } from '../types';
 import { ChooseMethodWrapper, ChooseSignUpSignInWrapper, CallToSignIn } from '../styles';
