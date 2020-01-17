ccf02eec7574fb2c914cef59fedc3a8d53dc8a34
gastonhero <gaston@hero-fintech.com>
2020-01-17 16:08:36 +0100

    fix tests

--- packages/testing/cypress/integration/login.spec.ts
+++ packages/testing/cypress/integration/login.spec.ts
@@ -3,14 +3,18 @@ describe('Login', function() {
     cy.web3('lender');
     cy.visit(`${Cypress.env('url')}/login`, { failOnStatusCode: false }); // int server returns 404 instead of 200 when a http request is done to the server if not /index
     cy.wait(2000);
-    cy.get('.process').matchImageSnapshot('login_modal');
+    cy.get('#btn-sign-in-email').click();
+    cy.wait(2000);
+    cy.get('#process').matchImageSnapshot('login_modal');
     const user = Cypress.env('user');
     cy.get('#input-login').should('have.length', 1);
     cy.get('#input-login').type(user['lender'].email);
     cy.get('#input-password').should('have.length', 1);
     cy.get('#input-password').type(user['lender'].password);
     cy.wait(1000);
-    cy.get('.process').matchImageSnapshot('login_modal_filled');
+    cy.get('#process').matchImageSnapshot('login_modal_filled');
     cy.get('#btn-login').click();
   });
 });
+
+
