c9a06a88adc92b73dae04bcecc4c011cc82ab55b
pol <>
2020-02-11 18:23:41 +0100

    LP-1056 added checkbox for membership on claim gains

--- packages/client/src/components/ClaimRepay/stages/Confirm.tsx
+++ packages/client/src/components/ClaimRepay/stages/Confirm.tsx
@@ -1,10 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Loader } from 'semantic-ui-react';
 import { Header, LenderButton } from '../../InvestModal/InvestModal.styles';
+import { CheckboxDeposit, CheckboxDepositLabel, CheckboxContainer } from '../styles';
 import { ClaimRepayContext } from '../ClaimRepay';
 import { getCalculations } from '../../../utils/loanUtils';
 import numeral from '../../../commons/numeral';
 import { fromWei } from 'web3-utils';
+
+import AppContext from '../../AppContext';
 import {
   ClaimFundsResume,
   FlexSpacedLayout,
@@ -29,12 +32,27 @@ const ResumeItemBig: React.SFC<ResumeItemProps> = ({ title, value }) => (
 
 const Confirm = () => {
   const { claimRepayment, loan, pending }: any = useContext(ClaimRepayContext);
+  const {
+    store: {
+      blockchain: { hasDeposit }
+    }
+  }: any = useContext(AppContext);
+  const [depositChecked, setDepositChecked] = useState(false);
+  const [membership, setMembership] = useState(null);
 
+  useEffect(() => {
+    console.log('has depoist::::: ', hasDeposit);
+    setMembership(hasDeposit);
+  }, [hasDeposit]);
   const { roi }: any = getCalculations(loan);
   const lenderAmount = numeral(fromWei(loan.lenderAmount)).format();
   const lenderRoiAmount = numeral(
     Number(fromWei(loan.lenderAmount)) + Number(fromWei(loan.lenderAmount)) * numeral(roi).value()
   ).format();
+
+  const toggle = () => {
+    setDepositChecked(!depositChecked);
+  };
 
   return (
     <>
@@ -45,6 +63,12 @@ const Confirm = () => {
           <ResumeItem title="ROI" value={`${roi}`} />
           <ResumeItemBig title="Investment return" value={`${lenderRoiAmount} DAI`} />
         </FlexSpacedLayout>
+        {!membership && (
+          <CheckboxContainer>
+            <CheckboxDeposit onChange={toggle} checked={depositChecked} />
+            <CheckboxDepositLabel>I wish to become become a member</CheckboxDepositLabel>
+          </CheckboxContainer>
+        )}
       </ClaimFundsResume>
       <Loader active inverted />
       <LenderButton loading={pending} onClick={claimRepayment}>
