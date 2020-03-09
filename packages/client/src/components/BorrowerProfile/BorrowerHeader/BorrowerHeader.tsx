1806ecc0e6b719e1c6527aef7e952c417fc73588
gastonhero <gaston@hero-fintech.com>
2020-03-09 12:01:17 +0100

    Quick fix loan

--- packages/client/src/components/BorrowerProfile/BorrowerHeader/BorrowerHeader.tsx
+++ packages/client/src/components/BorrowerProfile/BorrowerHeader/BorrowerHeader.tsx
@@ -25,7 +25,7 @@ const BorrowerHeader = ({ auction }) => {
       <LoanDaysLeft>
         <LoanDaysLeftWrapper>
           <LoanDaysLeftLabel>Days left</LoanDaysLeftLabel>
-          <LoanDaysLeftValue>{values && values.times.loanTermLeft}</LoanDaysLeftValue>
+          <LoanDaysLeftValue>{values && values.times.auctionTimeLeft}</LoanDaysLeftValue>
         </LoanDaysLeftWrapper>
       </LoanDaysLeft>
       <ProgressContainer>
