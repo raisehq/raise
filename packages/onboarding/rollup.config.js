3f5bbe24a5e2d02da192b1e7d0775763fcfc7f05
Daniel Martin <daniel@herotoken.io>
2020-03-10 19:24:08 +0100

    LP-1101 fix rollup

--- packages/onboarding/rollup.config.js
+++ packages/onboarding/rollup.config.js
@@ -1,11 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
 import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import filesize from 'rollup-plugin-filesize';
 import json from '@rollup/plugin-json';
 import localResolve from 'rollup-plugin-local-resolve';
-
+import builtins from 'rollup-plugin-node-builtins';
 import pkg from './package.json';
 
 const INPUT_FILE_PATH = 'src/index.tsx';
@@ -13,24 +14,43 @@ const OUTPUT_NAME = 'external';
 
 const GLOBALS = {
   react: 'React',
-  'react-dom': 'ReactDOM'
+  'react-dom': 'ReactDOM',
+  lodash: 'lodash'
 };
 
 const PLUGINS = [
+  builtins(),
+  resolve({
+    browser: true
+  }),
   json(),
   typescript(),
   babel({
     exclude: 'node_modules/**'
   }),
-  localResolve(),
-  resolve({
-    browser: true
-  }),
   commonjs({
     include: ['node_modules', 'node_modules/**', 'node_modules/**/*'],
     namedExports: {
       'node_modules/daggy/src/daggy.js': ['daggy'],
-      'node_modules/react-is/index.js': ['isElement', 'isValidElementType']
+      'node_modules/react-is/index.js': ['isElement', 'isValidElementType', 'isForwardRef'],
+      'node_modules/semantic-ui-react/node_modules/prop-types/index.js': [
+        'element',
+        'func',
+        'object',
+        'oneOfType'
+      ],
+      'node_modules/js-cookie/src/js.cookie.js': ['get', 'set'],
+      'node_modules/react-dom/index.js': ['render', 'createPortal', 'findDOMNode'],
+      'node_modules/prop-types/index.js': [
+        'oneOfType',
+        'func',
+        'object',
+        'string',
+        'shape',
+        'bool',
+        'element'
+      ],
+      'node_modules/lodash/lodash.js': ['debounce']
     }
   }),
   filesize()
