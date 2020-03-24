9fca5a7da6c791fee385df1034f4c61ddac52312
pol <>
2020-03-24 19:43:42 +0100

    LP-1134 updated toast messages

--- packages/client/src/helpers/toastMessages.ts
+++ packages/client/src/helpers/toastMessages.ts
@@ -2,38 +2,38 @@ const messages = {
   pending: {
     createLoan: 'New loan request',
     approval: 'Approve wallet operations',
-    withdrawLoan: 'Transfer {0} {1} to my wallet',
-    repayLoan: 'Repay loan amount with {0} {1}',
-    investLoan: 'Invest {0} {1}',
-    withdrawRefund: 'Get {0} {1} refund to my wallet',
+    withdrawLoan: 'Transferring the loan amount to your wallet',
+    repayLoan: 'Transferring the funds to repay the loan',
+    investLoan: 'Transferring {0} {1} to the loan',
+    withdrawRefund: 'Refunding invested amount to your wallet',
     withdrawRepaymentAndDeposit: 'Withdraw earnings to my wallet and make deposit',
-    withdrawRepayment: 'Withdraw {0} {1} earnings to my wallet ',
-    deposit: 'Deposit 200 Raise Tokens',
-    withdrawDeposit: 'Withdraw 200 Raise Tokens'
+    withdrawRepayment: 'Transferring your earnings to your wallet',
+    deposit: 'Processing the investor deposit',
+    withdrawDeposit: 'Transfering the deposit to your wallet'
   },
   success: {
-    createLoan: 'New loan request',
-    approval: 'Approve wallet operations',
-    withdrawLoan: 'Transfer {0} {1} to my wallet',
-    repayLoan: 'Repay loan amount with {0} {1}',
-    investLoan: 'Invest {0} {1}',
-    withdrawRefund: 'Get {0} {1} refund to my wallet',
+    createLoan: 'Done! Check your loan in the dashboard',
+    approval: 'Wallet operations approved',
+    withdrawLoan: 'Done! The funds have been transferred to your wallet',
+    repayLoan: 'Done! Your loan has been repaid',
+    investLoan: 'Done! Your investment has been placed',
+    withdrawRefund: 'The refund has been completed',
     withdrawRepaymentAndDeposit: 'Withdraw earnings to my wallet and make deposit',
-    withdrawRepayment: 'Withdraw {0} {1} earnings to my wallet ',
-    deposit: 'Deposit 200 Raise Tokens',
-    withdrawDeposit: 'Withdraw 200 Raise Tokens'
+    withdrawRepayment: 'Done! The funds have been transferred to your wallet',
+    deposit: 'Your deposit has been made',
+    withdrawDeposit: 'Your deposit has been withdrawn'
   },
   error: {
-    createLoan: 'New loan request',
-    approval: 'Approve wallet operations',
-    withdrawLoan: 'Transfer {0} {1} to my wallet',
-    repayLoan: 'Repay loan amount with {0} {1}',
-    investLoan: 'Invest {0} {1}',
-    withdrawRefund: 'Get {0} {1} refund to my wallet',
-    withdrawRepaymentAndDeposit: 'Withdraw earnings to my wallet and make deposit',
-    withdrawRepayment: 'Withdraw {0} {1} earnings to my wallet ',
-    deposit: 'Deposit 200 Raise Tokens',
-    withdrawDeposit: 'Withdraw 200 Raise Tokens'
+    createLoan: 'Something went wrong',
+    approval: 'Something went wrong',
+    withdrawLoan: 'Something went wrong',
+    repayLoan: 'Something went wrong',
+    investLoan: 'Something went wrong',
+    withdrawRefund: 'Something went wrong',
+    withdrawRepaymentAndDeposit: 'Something went wrong',
+    withdrawRepayment: 'Something went wrong',
+    deposit: 'Something went wrong',
+    withdrawDeposit: 'Something went wrong'
   }
 };
 
