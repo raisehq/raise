c6a7cd8f75ed8311b328928c561f226604e24d9e
gastonhero <gaston@hero-fintech.com>
2020-04-01 00:18:12 +0200

    Integrate with master. InvestState

--- packages/client/src/components/InvestModal/InvestState.tsx
+++ packages/client/src/components/InvestModal/InvestState.tsx
@@ -13,13 +13,13 @@ import { useAppContext } from '../../contexts/AppContext';
 import useGoogleTagManager, { TMEvents } from '../../hooks/useGoogleTagManager';
 import useGetCoin from '../../hooks/useGetCoin';
 import { useAddressBalance } from '../../contexts/BalancesContext';
+import { Button } from '@raisehq/components';
 
 import {
   Header,
   ModalInputContainer,
   ModalInputBox,
   InputLabel,
-  ConfirmButton,
   InputContainer,
   // Amount,
   // FundAllLabel,
@@ -194,9 +194,12 @@ const InvestState: React.SFC<InvestStateProps> = ({ loan, setStage, setInvestmen
         <LoanTermsCheckbox id="btn-check-term-condition-invest" onChange={onToggleTerms} />I agree
         to the Terms and Conditions of the Loan Agreement
       </CheckContainer>
-      <ConfirmButton
-        id="btn-invest-confirm"
+      <Button
+        idAttr="btn-invest-confirm"
         onClick={onConfirm}
+        text="Confirm"
+        type="secondary"
+        size="large"
         disabled={
           value === 0 ||
           value === undefined ||
@@ -205,9 +208,7 @@ const InvestState: React.SFC<InvestStateProps> = ({ loan, setStage, setInvestmen
           value > nMaxAmount ||
           kyc_status !== 3
         }
-      >
-        CONFIRM
-      </ConfirmButton>
+      />
     </>
   );
 };
