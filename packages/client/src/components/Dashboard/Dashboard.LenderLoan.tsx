f9c853f55bd95b05e0df670223f01d9860b51f36
pol <>
2020-04-07 17:07:18 +0200

    LP-1181 added fix to render state 6

--- packages/client/src/components/Dashboard/Dashboard.LenderLoan.tsx
+++ packages/client/src/components/Dashboard/Dashboard.LenderLoan.tsx
@@ -30,6 +30,8 @@ const Loan = ({ auction }: { auction: any }) => {
       () => <LenderACU auction={auction} calcs={calcs} />,
       [5, ANY],
       () => <LenderACU auction={auction} calcs={calcs} />,
+      [6, false],
+      () => <LenderExpired auction={auction} calcs={calcs} />,
       ANY,
       () => null
     );
