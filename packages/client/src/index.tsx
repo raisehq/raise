beab43f03b6879a7cc5756e5788dc9950da9ea63
Kartojal <canillas.mail@gmail.com>
2020-04-14 14:39:13 +0200

    LP-1207 loan ZenDesk after Help click

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -18,7 +18,7 @@ import BalancesContextProvider, {
 } from './contexts/BalancesContext';
 
 // Import some inline css
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './global.css';
