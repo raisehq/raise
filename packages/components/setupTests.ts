c098b7b20ff3bf290bd1df1e63d0f7a4be3997b1
Kartojal <canillas.mail@gmail.com>
2020-04-30 15:42:22 +0200

    LP-9999 added env CI

--- packages/components/setupTests.ts
+++ packages/components/setupTests.ts
@@ -3,6 +3,10 @@ import { setDefaultOptions } from 'jsdom-screenshot';
 
 // TravisCI and Linux OS require --no-sandbox to be able to run the tests
 // https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
+
+if (process.env.CI === 'true') {
+  console.log('Puppeter CI');
+}
 setDefaultOptions({
   launch: { args: process.env.CI === 'true' ? ['--no-sandbox'] : [] },
 });
