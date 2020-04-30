66425872ef2ff7b8aaaa3e3e3e9c527818960c9d
Kartojal <canillas.mail@gmail.com>
2020-04-30 15:15:54 +0200

    LP-9999 added photos and fix config

--- packages/components/setupTests.ts
+++ packages/components/setupTests.ts
@@ -1,3 +1,12 @@
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
+import { setDefaultOptions } from 'jsdom-screenshot';
+
+// TravisCI and Linux OS require --no-sandbox to be able to run the tests
+// https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-on-travis-ci
+setDefaultOptions({
+  launch: { args: process.env.CI === 'true' ? ['--no-sandbox'] : [] },
+});
+
+jest.setTimeout(10000);
 
 expect.extend({ toMatchImageSnapshot });
