6a3621ecd1c40dcdffc1f6eaf993800fdeaac6cb
gastonhero <gaston@hero-fintech.com>
2020-05-13 08:25:29 +0200

    Add progressbar2

--- packages/components/src/components/LoanHeader/LoanHeader.tsx
+++ packages/components/src/components/LoanHeader/LoanHeader.tsx
@@ -23,52 +23,57 @@ const LoanHeader = ({
   raisedAmount,
   targetAmount,
   raiseInvestmentAmount,
-}) => (
-  <Wrapper>
-    <Row>
-      <Logo>
-        <ImageLogo
-          src={`${process.env.REACT_APP_HOST_IMAGES}/images/${logo}`}
-        />
-      </Logo>
-      <Timer>{endAuctionDate} </Timer>
-    </Row>
-    <Row>
-      <div>
-        <Label>Raised so far</Label>
-        <Value>{raisedAmount}</Value>
-      </div>
-      <div>
-        <RightLabel>Target</RightLabel>
-        <Value>{targetAmount}</Value>
-      </div>
-    </Row>
-    <Row>
-      <ProgressBar>
-        <WrapperFiller>
-          <RaisedSofarFiller>
-            <WrapperFiller>
-              <RaiseFiller />
-              <TextRaiseFiller>
-                <span>{`${raiseInvestmentAmount}% Raise`}</span>
-              </TextRaiseFiller>
-            </WrapperFiller>
-          </RaisedSofarFiller>
-          <TextRaisedSofarFiller>
-            <span>70% Total</span>
-          </TextRaisedSofarFiller>
-        </WrapperFiller>
-      </ProgressBar>
-    </Row>
-  </Wrapper>
-);
+}) => {
+  const getTotal = amount => target =>
+    (parseInt(amount, 10) / parseInt(target, 10)) * 100;
+
+  return (
+    <Wrapper>
+      <Row>
+        <Logo>
+          <ImageLogo
+            src={`${process.env.REACT_APP_HOST_IMAGES}/images/${logo}`}
+          />
+        </Logo>
+        <Timer>{endAuctionDate}</Timer>
+      </Row>
+      <Row>
+        <div>
+          <Label>Raised so far</Label>
+          <Value>{raisedAmount}</Value>
+        </div>
+        <div>
+          <RightLabel>Target</RightLabel>
+          <Value>{targetAmount}</Value>
+        </div>
+      </Row>
+      <Row>
+        <ProgressBar>
+          <WrapperFiller>
+            <RaisedSofarFiller>
+              <WrapperFiller>
+                <RaiseFiller />
+                <TextRaiseFiller>
+                  <span>{`${raiseInvestmentAmount}% Raise`}</span>
+                </TextRaiseFiller>
+              </WrapperFiller>
+            </RaisedSofarFiller>
+            <TextRaisedSofarFiller>
+              <span>{`${getTotal(raisedAmount)(targetAmount)}% Total`}</span>
+            </TextRaisedSofarFiller>
+          </WrapperFiller>
+        </ProgressBar>
+      </Row>
+    </Wrapper>
+  );
+};
 
 LoanHeader.propTypes = {
-  logo: PropTypes.string,
-  endAuctionDate: PropTypes.string,
-  raisedAmount: PropTypes.string,
-  targetAmount: PropTypes.string,
-  raiseInvestmentAmount: PropTypes.string,
+  logo: PropTypes.string.isRequired,
+  endAuctionDate: PropTypes.string.isRequired,
+  raisedAmount: PropTypes.string.isRequired,
+  targetAmount: PropTypes.string.isRequired,
+  raiseInvestmentAmount: PropTypes.string.isRequired,
 };
 
 export default LoanHeader;
