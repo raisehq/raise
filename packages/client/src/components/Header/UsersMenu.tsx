b9fc307d103170ba5ac099ae1df0f0abb63edb07
Kartojal <canillas.mail@gmail.com>
2020-05-14 16:38:54 +0200

    LP-1319 single file to separate styles from file

--- packages/client/src/components/Header/UsersMenu.tsx
+++ packages/client/src/components/Header/UsersMenu.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Button } from '@raisehq/components';
-import { ReactLink } from '../Links';
+import { LinkReactButton } from './UsersMenu.styles';
 import { useRootContext } from '../../contexts/RootContext';
+import Web3Address from '../Web3Address/Web3Address';
 import MyAccountButton from '../DesktopHeader/MyAccountButton';
 import { Balance as HeaderBalance } from '../HeaderBalance';
-
-const LinkReactButton = styled(ReactLink)`
-  display: flex;
-  align-items: center;
-  text-align: center;
-  padding: 12px 16px;
-  border: 1px solid #b1b3b9;
-  box-sizing: border-box;
-  border-radius: 3px;
-  font-size: 16px;
-  color: #3c4251;
-  font-weight: bold;
-`;
 
 const UsersMenu = () => {
   const {
@@ -37,6 +24,9 @@ const UsersMenu = () => {
   return (
     <>
       {CreateLoanButton}
+      <div>
+        <Web3Address border={false} />
+      </div>
       <HeaderBalance />
       <MyAccountButton />
       <Button size="standard" type="tertiary" onClick={onSignout} text="Log out" />
---
+++ packages/client/src/components/Header/VisitorsMenu.styles.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { Button } from '@raisehq/components';
+
+export const SignupButton = styled(Button)`
+  &&& {
+    margin-left: 16px;
+  }
+`;
+
+export default SignupButton;
