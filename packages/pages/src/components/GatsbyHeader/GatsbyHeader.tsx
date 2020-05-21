32cd5622ecf8c6469baddb99398f91959b99a374
gastonhero <gaston@hero-fintech.com>
2020-05-21 10:42:57 +0200

    LP-1376:Change conditional rendering for media queries

--- packages/pages/src/components/GatsbyHeader/GatsbyHeader.tsx
+++ packages/pages/src/components/GatsbyHeader/GatsbyHeader.tsx
@@ -8,38 +8,35 @@ import routes from '../../routes';
 
 const REACT_APP_HOST_URL = process.env.REACT_APP_HOST_URL; // eslint-disable-line
 
-const mobileView = (
-  <MobileLinkWrapper>
-    <LinkContainer>
-      <a href={`${REACT_APP_HOST_URL}/login`}>Login</a>
-    </LinkContainer>
-    <LinkContainer>
-      <a href={`${REACT_APP_HOST_URL}/join`}>Sign Up</a>
-    </LinkContainer>
-  </MobileLinkWrapper>
-);
-
-const desktopView = (
-  <ButtonWrapper>
-    <Button
-      title="Log in"
-      as={BasicLink}
-      to={`${REACT_APP_HOST_URL}/login`}
-      type="tertiary"
-      size="small"
-    />
-    <Button
-      title="Sign up"
-      as={BasicLink}
-      to={`${REACT_APP_HOST_URL}/join`}
-      type="secondary"
-      size="small"
-    />
-  </ButtonWrapper>
-);
-
 const GatsbyHeader = () => {
-  return <RaiseHeader {...routes}>{isMobile ? mobileView : desktopView}</RaiseHeader>;
+  return (
+    <RaiseHeader {...routes}>
+      <MobileLinkWrapper>
+        <LinkContainer>
+          <a href={`${REACT_APP_HOST_URL}/login`}>Login</a>
+        </LinkContainer>
+        <LinkContainer>
+          <a href={`${REACT_APP_HOST_URL}/join`}>Sign Up</a>
+        </LinkContainer>
+      </MobileLinkWrapper>
+      <ButtonWrapper>
+        <Button
+          title="Log in"
+          as={BasicLink}
+          to={`${REACT_APP_HOST_URL}/login`}
+          type="tertiary"
+          size="small"
+        />
+        <Button
+          title="Sign up"
+          as={BasicLink}
+          to={`${REACT_APP_HOST_URL}/join`}
+          type="secondary"
+          size="small"
+        />
+      </ButtonWrapper>
+    </RaiseHeader>
+  );
 };
 
 export default GatsbyHeader;
