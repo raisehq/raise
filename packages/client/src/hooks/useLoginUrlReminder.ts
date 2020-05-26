924fbd204731e6c1dd69b2a306b57967a676bfd0
Kartojal <canillas.mail@gmail.com>
2020-05-26 14:40:07 +0200

    LP-1373 add blacklist to prevent saving web3-check and /join to save. This allows visitors to log at /account and come back to the account page after login.

--- packages/client/src/hooks/useLoginUrlReminder.ts
+++ packages/client/src/hooks/useLoginUrlReminder.ts
@@ -3,24 +3,30 @@ import LocalData from '../helpers/localData';
 import useRouter from './useRouter';
 import useCookie from './useCookie';
 
+const blackList = ['/join', '/verify-web3'];
+
+const passBlackList = (pathToCheck: string) =>
+  !!pathToCheck && !blackList.some((blackPath) => pathToCheck.includes(blackPath));
+
 const useLoginUrlReminder = () => {
   const [savedPath, setPath] = useCookie('savedPath', '');
   const {
     push,
     location: { pathname, search }
   } = useRouter();
+  const fullPath = `${pathname}${search}`;
 
   const logged = !!LocalData.get('auth');
 
   useEffect(() => {
     if (!logged) {
-      const fullPath = `${pathname}${search}`;
-      if (fullPath !== savedPath) {
-        setPath(`${pathname}${search}`);
+      if (fullPath !== savedPath && passBlackList(fullPath)) {
+        setPath(fullPath);
       }
       return;
     }
-    if (logged && savedPath && savedPath.includes('/')) {
+    if (logged && savedPath && savedPath.includes('/') && fullPath !== savedPath) {
+      console.log('pusshing', fullPath, savedPath);
       setPath('');
       push(savedPath);
     }
