add6777573ca0abf82751b1c79f86c12158b1a7f
gastonhero <gaston@hero-fintech.com>
2020-06-02 10:02:52 +0200

    Link button with buyCrypto screen

--- packages/client/src/components/HeaderBalance/Balance.styles.ts
+++ packages/client/src/components/HeaderBalance/Balance.styles.ts
@@ -1,14 +1,10 @@
 import styled from 'styled-components';
-import { Button as RaiseButton } from '@raisehq/components';
+
 import { Dropdown, Divider as DividerRaw, Button, ButtonProps } from 'semantic-ui-react';
 import TokenBalanceNotStyled from './TokenBalance';
 import TokenLayoutNotStyled from './TokenLayout';
 import { device } from '../../commons/breakpoints';
 import { AddressStatus as AddressStatusRaw } from '../Web3Address';
-
-interface TrackingButtonProps {
-  isDisabled: boolean;
-}
 
 export const TokenBalance = styled(TokenBalanceNotStyled)``;
 export const TokenLayout = styled(TokenLayoutNotStyled)``;
@@ -137,19 +133,4 @@ export const ButtonContainer = styled.div`
   align-items: center;
   width: 100%;
   padding: 15px;
-`;
-
-export const TrackingButtonWrapper = styled(RaiseButton)<TrackingButtonProps>`
-  &&&&& {
-    ${({ isDisabled }) => isDisabled && 'background-color: #ffba00'};
-    ${({ isDisabled }) => isDisabled && 'border-color:  #ffba00'};
-    cursor: pointer;
-
-    &&&&&:focus,
-    &&&&&:hover {
-      ${({ isDisabled }) => isDisabled && 'background-color: #ffba00'};
-      ${({ isDisabled }) => isDisabled && 'border-color:  #ffba00'};
-      ${({ isDisabled }) => isDisabled && 'box-shadow:  none'};
-    }
-  }
 `;
