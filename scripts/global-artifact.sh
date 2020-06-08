04447acc8121e5888c84628c290472ac8267b998
Daniel Martin <daniel@raise.it>
2020-06-08 13:06:19 +0200

    LP-1398 build and checks

--- scripts/global-artifact.sh
+++ scripts/global-artifact.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+DIR_ONBOARDING="/packages/onboarding/dist"
+if [ ! -d "$DIR_ONBOARDING" ]; then
+  aws s3 cp --recursive s3://raise-actirfact-pipeline/onboarding /packages/onboarding/dist  
+fi
+
+DIR_COMPONENTS="/packages/components/dist"
+if [ ! -d "$DIR_COMPONENTS" ]; then
+  aws s3 cp --recursive s3://raise-actirfact-pipeline/components /packages/components/dist  
+fi
