28bf1cc98593ec67085dbef0662095bbb14edd83
Daniel Martin <daniel@raise.it>
2020-06-08 13:28:24 +0200

    LP-1398 path

--- scripts/global-artifact.sh
+++ scripts/global-artifact.sh
@@ -2,10 +2,10 @@
 
 DIR_ONBOARDING="/packages/onboarding/dist"
 if [ ! -d "$DIR_ONBOARDING" ]; then
-  aws s3 cp --recursive s3://raise-actirfact-pipeline/onboarding /packages/onboarding/dist  
+  aws s3 cp --recursive s3://raise-actirfact-pipeline/onboarding/ /packages/onboarding/dist  
 fi
 
 DIR_COMPONENTS="/packages/components/dist"
 if [ ! -d "$DIR_COMPONENTS" ]; then
-  aws s3 cp --recursive s3://raise-actirfact-pipeline/components /packages/components/dist  
+  aws s3 cp --recursive s3://raise-actirfact-pipeline/components/ /packages/components/dist  
 fi
