d1dee15563d3f42745e23be608e58660a18ee4b1
Francesc Armengol <francesc@raise.it>
2020-06-12 14:03:03 +0200

    LP-1431: Remove env

--- packages/client/src/helpers/datadogLogs.ts
+++ packages/client/src/helpers/datadogLogs.ts
@@ -3,7 +3,6 @@ import { datadogLogs } from '@datadog/browser-logs';
 datadogLogs.init({
   clientToken: process.env.REACT_APP_DATADOG || '',
   datacenter: 'eu',
-  env: process.env.NODE_ENV,
   forwardErrorsToLogs: true,
   sampleRate: 100
 });
