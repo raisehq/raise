28ac021cf9ac175904aa869644cfbf4fdd43d10f
gastonhero <gaston@hero-fintech.com>
2020-06-15 13:15:37 +0200

    Modification3. Fix

--- packages/pages/src/components/BenefitsSection/BenefitsSection.tsx
+++ packages/pages/src/components/BenefitsSection/BenefitsSection.tsx
@@ -109,13 +109,12 @@ const BenefitsSection = () => {
         </ImageWrapper>
         <Column>
           <StepWrapper>
-            {selectedOption === 1
-              ? stepsInvestors.map((item) => (
-                  <Step number={`0${item.number}`} text={item.text} key={item.number} />
-                ))
-              : stepsBorrowers.map((item) => (
-                  <Step number={`0${item.number}`} text={item.text} key={item.number} />
-                ))}
+            {stepsInvestors.map((item) => (
+              <Step number={`0${item.number}`} text={item.text} key={item.number} visible={selectedOption === 1} />
+            ))}
+            {stepsBorrowers.map((item) => (
+              <Step number={`0${item.number}`} text={item.text} key={item.number} visible={selectedOption !== 1} />
+            ))}
           </StepWrapper>
           <CheckLoanText href={`${process.env.REACT_APP_HOST_URL}`}>
             <span>{linkText}</span>
