6265abccc51d2712f4ce324ab2ba9d969d09144e
Francesc Armengol <francesc@raise.it>
2020-06-15 16:23:10 +0200

    Debugging

--- packages/client/src/helpers/datadogLogs.ts
+++ packages/client/src/helpers/datadogLogs.ts
@@ -1,12 +1,12 @@
 import { datadogLogs } from '@datadog/browser-logs';
 
-console.log('Client token:::', process.env.REACT_APP_DATADOG);
-
 datadogLogs.init({
   clientToken: process.env.REACT_APP_DATADOG || '',
   datacenter: 'eu',
   forwardErrorsToLogs: true,
   sampleRate: 100
 });
+
+datadogLogs.logger.info('Datadog init', { name: 'datadogInit', id: 1 });
 
 export default datadogLogs;
