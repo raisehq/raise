e827cc2edba02212cc0438f59e222ac94b4815c2
Francesc Armengol <francesc@raise.it>
2020-06-16 12:41:38 +0200

    Found bug 403 forbbiden with datadog datacenter. If you have an account in us you have to get the us datacenter. The same for europe.

--- packages/client/src/helpers/datadogLogs.ts
+++ packages/client/src/helpers/datadogLogs.ts
@@ -2,9 +2,10 @@ import { datadogLogs } from '@datadog/browser-logs';
 
 datadogLogs.init({
   clientToken: process.env.REACT_APP_DATADOG || '',
-  datacenter: 'eu',
+  datacenter: 'us',
   forwardErrorsToLogs: true,
-  sampleRate: 100
+  sampleRate: 100,
+  env: process.env.NODE_ENV
 });
 
 datadogLogs.logger.info('Datadog init', { name: 'datadogInit', id: 1 });
