7c783e347706a29f2f97acbe9b40bbedbb3f9471
gastonhero <gaston@hero-fintech.com>
2020-07-02 15:20:17 +0200

    Add data

--- packages/storybook/stories/BenefitsSection.stories.tsx
+++ packages/storybook/stories/BenefitsSection.stories.tsx
@@ -18,21 +18,27 @@ const data = [
     main_title: 'Benefits',
     image: 'https://cdn.buttercms.com/kKvWSNpTRar6thqQDs9Q',
     id: 'investors',
-    step_one: 'aaaa',
-    step_two: 'aaaaa',
-    step_three: 'aaaa',
-    text_link_to_app: 'Check loan',
-    link_to_app: '',
+    step_one:
+      'Generate passive income that will give you around 10% annually by investing in our loan offers',
+    step_two:
+      'Raise connects investors and businesses through smart contracts to deliver a safe and transparent experience',
+    step_three:
+      'Find opportunities to invest in business loans from anywhere in the world using borderless currencies',
+    text_link_to_app: 'Check loan offer',
+    link_to_app: 'https://app.raise.it',
   },
   {
     main_title: 'Benefits',
     image: 'https://cdn.buttercms.com/PfSLk7C0SjyXTUpUzFpL',
     id: 'borrowers',
-    step_one: 'bbbb',
-    step_two: 'bbbb',
-    step_three: 'bbbb',
-    text_link_to_app: 'Check loan',
-    link_to_app: '',
+    step_one:
+      'Borrow money to grow your business at an interest rate and loan term you choose',
+    step_two:
+      "You won't be required any collateral. Raise evaluates your repayment capacity based on your business financials",
+    step_three:
+      'Get a loan in an stablecoin for a period from 1 day to 1 year. Early loan repayment without extra fees and penalties',
+    text_link_to_app: 'Become a borrower',
+    link_to_app: 'https://borrow.raise.it',
   },
 ];
 
